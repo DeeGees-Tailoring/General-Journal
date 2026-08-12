6bf470acc731b219440d4458e7866a7b4047a539
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-12 21:45:30 +0800

    Add invoice#00001

--- entries.js
+++ entries.js
@@ -6,6 +6,15 @@ const invoices = {
         Repair: "",
         Alteration: "",
         InvAmount: ""
+    },
+
+    "invoice#00001": {
+        Date: "2026-08-12",
+        Customer: "AIDA TOKS",
+        Address: "Brgy 4, Ma. Aurora, Aurora",
+        Repair: "150",
+        Alteration: "",
+        InvAmount: "150"
     }
 
 };
