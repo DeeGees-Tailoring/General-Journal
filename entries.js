80ff5b6ea96aab1f96991406b005957915f2161f
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-12 19:19:31 +0800

    Add invoice#00002

--- entries.js
+++ entries.js
@@ -15,6 +15,15 @@ const invoices = {
         Repair: "300",
         Alteration: "",
         InvAmount: "300"
+    },
+
+    "invoice#00002": {
+        Date: "2026-08-12",
+        Customer: "HILDA LORILLA",
+        Address: "Ghkl",
+        Repair: "60",
+        Alteration: "",
+        InvAmount: "60"
     }
 
 };
