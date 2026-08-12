c0ffdf06d28dce37e7b803027fd8eef4bebb65a4
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-12 17:47:57 +0800

    Update entries.js

--- entries.js
+++ entries.js
@@ -1,35 +1,12 @@
 const invoices = {
         
     "invoice#00000": {
+        Date: "",
         Customer: "",
         Address: "",
         Repair: "",
         Alteration: "",
         InvAmount: ""
-    },
-
-    "invoice#00001": {
-        Customer: "HILDA LORILLA",
-        Address: "Brgy 4, Ma. Aurora, Aurora",
-        Repair: "50",
-        Alteration: "100",
-        InvAmount: "150"
-    },
-
-    "invoice#00002": {
-        Customer: "AIDA TOKS",
-        Address: "Brgy 4, Ma. Aurora, Aurora",
-        Repair: "100",
-        Alteration: "",
-        InvAmount: "100"
-    },
-
-    "invoice#00003": {
-        Customer: "Vivian Silvestre",
-        Address: "Brgy 4, Ma. Aurora, Aurora",
-        Repair: "",
-        Alteration: "150",
-        InvAmount: "150"
     }
 
 };
