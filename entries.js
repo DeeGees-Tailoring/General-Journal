9ed53e10d79a5f7fd9cc6c2a4b24a10dd6c59270
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-12 21:42:51 +0800

    Update entries.js

--- entries.js
+++ entries.js
@@ -6,24 +6,6 @@ const invoices = {
         Repair: "",
         Alteration: "",
         InvAmount: ""
-    },
-
-    "invoice#00001": {
-        Date: "2026-08-12",
-        Customer: "HILDA LORILLA",
-        Address: "Rtuk",
-        Repair: "300",
-        Alteration: "",
-        InvAmount: "300"
-    },
-
-    "invoice#00002": {
-        Date: "2026-08-12",
-        Customer: "HILDA LORILLA",
-        Address: "Ghkl",
-        Repair: "60",
-        Alteration: "",
-        InvAmount: "60"
     }
 
 };
