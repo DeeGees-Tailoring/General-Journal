c9fd8dfff0bfe3578cb8ba99f3936fd69245cec4
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-15 15:12:01 +0800

    Add CD#00001 to cash disbursements

--- cashdisbursements.js
+++ cashdisbursements.js
@@ -1,3 +1,10 @@
 const cashDisbursements = {
-    
+    "CD#00001": {
+        Date: "2026-08-15",
+        Payee: "AURELCO",
+        Category: "Expense",
+        Description: "Electricity",
+        Amount: "1290.58"
+    }
+
 };
