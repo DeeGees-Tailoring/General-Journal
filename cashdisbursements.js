1a17b82981c5607b3ee7ab4cd464f382cbdbe6a1
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-15 16:07:48 +0800

    Update cashdisbursements.js

--- cashdisbursements.js
+++ cashdisbursements.js
@@ -1,18 +1,4 @@
 const cashDisbursements = {
-    "CD#00001": {
-        Date: "2026-08-15",
-        Payee: "AURELCO",
-        Category: "Expense",
-        Description: "Electricity",
-        Amount: "1290.58"
-    },
-
-    "CD#00002": {
-        Date: "2026-08-16",
-        Payee: "AURELCO",
-        Category: "Expense",
-        Description: "Kuryente",
-        Amount: "100"
-    }
+    
 
 };
