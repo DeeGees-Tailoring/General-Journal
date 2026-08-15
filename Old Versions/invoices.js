9ff46195c14c0d73dd0e8fed6b52d8e3d2f539a2
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-15 12:48:16 +0800

    Create invoices.js

---
+++ Old Versions/invoices.js
@@ -0,0 +1,47 @@
+const invoices = {
+    "invoice#04622": {
+        Date: "2026-08-12",
+        Customer: "AIDA TOKS",
+        Address: "Brgy 4, Ma. Aurora, Aurora",
+        Repair: "150",
+        Alteration: "",
+        InvAmount: "150"
+    },
+
+    "invoice#04623": {
+        Date: "2026-08-13",
+        Customer: "Sheila Catipon",
+        Address: "San Jose, Ma. Aurora, Aurora",
+        Repair: "80",
+        Alteration: "",
+        InvAmount: "80"
+    },
+
+    "invoice#04624": {
+        Date: "2026-08-13",
+        Customer: "Editha Riaza",
+        Address: "Brgy 4, Ma. Aurora, Aurora",
+        Repair: "",
+        Alteration: "100",
+        InvAmount: "100"
+    },
+
+    "invoice#04625": {
+        Date: "2026-08-14",
+        Customer: "Sonia Dulay",
+        Address: "Florida, Ma. Aurora, Aurora",
+        Repair: "",
+        Alteration: "100",
+        InvAmount: "100"
+    },
+
+    "invoice#04626": {
+        Date: "2026-08-14",
+        Customer: "Julita Valdez",
+        Address: "Brgy 2, Ma. Aurora, Aurora",
+        Repair: "100",
+        Alteration: "",
+        InvAmount: "100"
+    }
+
+};
