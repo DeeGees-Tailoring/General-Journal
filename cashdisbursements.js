464e9531c80512740fe10fb699cfa5b8f7934754
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-16 02:46:32 +0800

    Update cashdisbursements.js

--- cashdisbursements.js
+++ cashdisbursements.js
@@ -2,7 +2,7 @@ const cashDisbursements = {
     "CD#00001": {
         Date: "2026-08-12",
         Payee: "AURELCO",
-        Category: "Expense - Electricity",
+        Category: "Expense (Electricity)",
         Description: "August bill",
         Amount: "1290.58"
     }
