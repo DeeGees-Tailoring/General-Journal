0ee5b35f54778aaee4e0c44d7468185a3b1a67bc
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-15 13:27:36 +0800

    Add Invoice#04627 to invoices

--- invoices.js
+++ invoices.js
@@ -42,6 +42,15 @@ const invoices = {
         Repair: "100",
         Alteration: "",
         InvAmount: "100"
+    },
+
+    "Invoice#04627": {
+        Date: "2026-08-15",
+        Customer: "HILDA LORILLA",
+        Address: "Hhkl",
+        Repair: "100",
+        Alteration: "",
+        InvAmount: "100"
     }
 
 };
