bb3ae0c33cece4c4a74f676ab59902e4ec10f1f8
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-15 13:48:02 +0800

    Update entries.js

--- entries.js
+++ entries.js
@@ -1,18 +1,3 @@
 const entries = {
-    "Invoice#04627": {
-        Type: "Invoice",
-        Date: "2026-08-15",
-        Category: "Income",
-        Description: "100",
-        Amount: "100"
-    },
-
-    "Invoice#04628": {
-        Type: "Invoice",
-        Date: "2026-08-15",
-        Category: "Income",
-        Description: "100",
-        Amount: "100"
-    }
 
 };
