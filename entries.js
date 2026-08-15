7f412eb7d8d8860c44187356df06005a21fa71b1
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-15 12:50:00 +0800

    Create entries.js

---
+++ entries.js
@@ -0,0 +1,17 @@
+const entries = {
+    "Invoice#00000": {
+        Type: "Invoice",
+        Date: "2026-08-15",
+        Category: "Income",
+        Description: "Repair / Alteration",
+        Amount: "850"
+    },
+
+    "Bill#00000": {
+        Type: "Bill",
+        Date: "2026-08-15",
+        Category: "Expense",
+        Description: "Electricity bill",
+        Amount: "850"
+    }
+};
