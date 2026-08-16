7bae0aed5e371708f0ce3b74b541af0166f59bda
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-16 16:08:12 +0800

    Add Invoice#04632 to journal

--- entries.js
+++ entries.js
@@ -85,6 +85,14 @@ const entries = {
         Category: "Income",
         Description: "150",
         Amount: "150"
+    },
+
+    "Invoice#04632": {
+        Type: "Invoice",
+        Date: "2026-08-16",
+        Category: "Income",
+        Description: "70",
+        Amount: "70"
     }
 
 };
