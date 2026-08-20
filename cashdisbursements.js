18dae94ed77a0d9f79d818b48a69ffe5258a7ce7
DeeGees-Tailoring <deegeesv1@gmail.com>
2026-08-20 11:09:04 +0800

    Add CD#00002 to cash disbursements

--- cashdisbursements.js
+++ cashdisbursements.js
@@ -5,6 +5,14 @@ const cashDisbursements = {
         Category: "Expense (Electricity)",
         Description: "August bill",
         Amount: "1290.58"
+    },
+
+    "CD#00002": {
+        Date: "2026-08-20",
+        Payee: "Municipal Treasurer's Office (MTO)",
+        Category: "Expense (Market Stall Fee)",
+        Description: "OR#4927288T",
+        Amount: "1152"
     }
 
 };
